c521d0a261e9421f0f4c8161abd7e82293a8c475
Ayomide <heyayomideadebisi@gmail.com>
2026-03-04 21:00:35 +0100

    cleanup client clode

--- apps/client/src/app/main.tsx
+++ apps/client/src/app/main.tsx
@@ -4,18 +4,15 @@ import App from "./App.tsx";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "@/shared/hooks/useTheme.tsx";
 import { NuqsAdapter } from "nuqs/adapters/react";
-// import { Toaster } from "sonner";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {/* <AuthProvider> */}
       <NuqsAdapter>
         <App />
         <ToastContainer />
       </NuqsAdapter>
-      {/* </AuthProvider> */}
     </ThemeProvider>
   </HelmetProvider>,
 );
--- apps/client/src/shared/state/welcome.ts
+++ apps/client/src/shared/state/welcome.ts
@@ -4,8 +4,10 @@ import { createJSONStorage, persist } from "zustand/middleware";
 interface WelcomeState {}
 
 export const useWelcomeState = create<WelcomeState>()(
-  persist((set) => ({}), {
-    name: "t7est",
-    storage: createJSONStorage(() => localStorage),
-  }),
+  persist(
+    (set) => ({
+      user: null,
+    }),
+    { name: "t7est", storage: createJSONStorage(() => localStorage) },
+  ),
 );
