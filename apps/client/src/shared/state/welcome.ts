432650135544fcbd4aa50285d6e92f9e6685ee10
Ayomide <heyayomideadebisi@gmail.com>
2026-03-04 20:56:02 +0100

    update welcome state types

--- apps/client/src/shared/state/welcome.ts
+++ apps/client/src/shared/state/welcome.ts
@@ -1,15 +1,11 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-interface WelcomeState {
-  user: null;
-}
+interface WelcomeState {}
 
 export const useWelcomeState = create<WelcomeState>()(
-  persist(
-    (set) => ({
-      user: null,
-    }),
-    { name: "t7est", storage: createJSONStorage(() => localStorage) },
-  ),
+  persist((set) => ({}), {
+    name: "t7est",
+    storage: createJSONStorage(() => localStorage),
+  }),
 );
