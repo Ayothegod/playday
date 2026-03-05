a2577dc29bb4d53dc8e3d8120cdfe66b47aec6cd
Ayomide <heyayomideadebisi@gmail.com>
2026-03-06 00:36:40 +0100

    add axios instance and auth types

---
+++ apps/client/src/shared/lib/types.ts
@@ -0,0 +1,21 @@
+interface CustomAxiosErrorResponse {
+  message: string;
+  statusCode: number;
+}
+
+export interface AxiosErrorResponse extends CustomAxiosErrorResponse {
+  errors: { field: string; message: string }[];
+  stack: null;
+}
+
+// interface BaseAPIResponse {
+//   message: string;
+//   statusCode: number;
+//   success?: boolean;
+// }
+
+// export interface GetMeResponse extends BaseAPIResponse {
+//   data: {
+//     user: User;
+//   };
+// }
